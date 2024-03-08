4819fc9f1464ebfa11087850e63ae748b764d333
Amir Reza Qasemi <83636447+AR-Q@users.noreply.github.com>
2024-03-08 19:27:52 +0330

    Finish Task1

--- answers/40130112093/task1.py
+++ answers/40130112093/task1.py
@@ -2,38 +2,23 @@ import numpy as np
 
 one_d_array = np.array([4,5,6,6,4,2,2])
 
-print(one_d_array)
+print(one_d_array.ndim)
 print(np.shape(one_d_array))
 print(one_d_array.dtype)
 print(np.sum(one_d_array))
 print("-----------------------------")
 
-two_d_array = np.array([
-    [2,5,1,6],
-    [4,6,6,2],
-    [7,9,0,1]
-])
+two_d_array = np.zeros((3,4))
 
-print(two_d_array)
+print(two_d_array.ndim)
 print(np.shape(two_d_array))
 print(two_d_array.dtype)
 print(np.sum(two_d_array))
 print("-----------------------------")
 
-three_d_array = np.array([
-    [
-        [2,5,1,6],
-        [4,6,6,2],
-        [7,9,0,1]
-    ],
-    [
-        [12,42,16,10],
-        [57,44,86,23],
-        [35,61,12,30]
-    ]
-])
+three_d_array = np.ones((2,3,4))
 
-print(three_d_array)
+print(three_d_array.ndim)
 print(np.shape(three_d_array))
 print(three_d_array.dtype)
 print(np.sum(three_d_array))
