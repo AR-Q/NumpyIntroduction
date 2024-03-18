0ad70fee6aa46b89e86ca1818dfd77070d3a807c
Amir Reza Qasemi <83636447+AR-Q@users.noreply.github.com>
2024-03-18 22:22:26 +0330

    finish task2 part1

---
+++ answers/40130112093/task2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+one_d_array = np.array([4,5,6,6,4,2,2])
+
+print(one_d_array[2])
+
+two_d_array = np.array([
+    [1,2,3,4],
+    [4,5,6,7],
+    [7,8,9,10]
+])
+
+print(two_d_array[2][3])
