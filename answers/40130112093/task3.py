c0a856f16570000f853f3acfd2135fa18b1aa62e
Amir Reza Qasemi <83636447+AR-Q@users.noreply.github.com>
2024-03-18 22:46:44 +0330

    add Task3 part1

--- answers/40130112093/task3.py
+++ answers/40130112093/task3.py
@@ -1,2 +1,21 @@
 import numpy as np
 
+a = np.array([
+    [1,2],
+    [3,4]
+])
+
+b = np.array([
+    [2,3],
+    [4,5]
+])
+
+sum = np.add(a,b)
+sub = np.subtract(a,b)
+mul = np.multiply(a,b)
+div = np.divide(a,b)
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
