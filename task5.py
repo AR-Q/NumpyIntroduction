590e08238da4143c61e7979cc07d0a9684506cb7
Amir Reza Qasemi <83636447+AR-Q@users.noreply.github.com>
2024-03-19 00:21:32 +0330

    add Task5 part1

--- task5.py
+++ task5.py
@@ -1,2 +1,27 @@
 import numpy as np
 
+a = np.array([
+    [1,2,3,4],
+    [4,5,6,7],
+    [7,8,9,10]
+])
+
+
+mean = np.mean(a)
+
+print(mean)
+
+
+median = np.median(a)
+
+print(median)
+
+
+variance = np.var(a)
+
+print(variance)
+
+
+std = np.std(a)
+
+print(std)
